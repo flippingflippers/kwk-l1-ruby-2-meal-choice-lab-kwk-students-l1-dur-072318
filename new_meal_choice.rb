6c0a5ddec7c0a442c40f461f6ca13f8fca4e9777
flippingflippers <citrussunshine2004@gmail.com>
2018-07-24 14:41:17 +0000

    automatically backed up by learn

---
+++ new_meal_choice.rb
@@ -0,0 +1,6 @@
+def breakfast (default_food= "frosted flakes")
+  puts "Morning is the most spectacular time of the day for nice healthy #{default_food}"
+end
+
+def lunch (default_food= "turkey sandwhich")
+  puts "Around 12'o clock thats the time when you need to "
