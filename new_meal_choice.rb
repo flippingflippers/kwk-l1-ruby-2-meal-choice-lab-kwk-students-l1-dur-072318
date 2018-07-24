2d9fdc8510e288a2a5ec739809a3f3a32ce3c834
flippingflippers <citrussunshine2004@gmail.com>
2018-07-24 14:45:28 +0000

    automatically backed up by learn

--- new_meal_choice.rb
+++ new_meal_choice.rb
@@ -3,4 +3,18 @@ def breakfast (default_food= "frosted flakes")
 end
 
 def lunch (default_food= "turkey sandwhich")
-  puts "Around 12'o clock thats the time when you need to "
+  puts "Around 12'o clock thats the time when you need to re energize and fill up on some #{default_food}"
+end
+
+def snack (default_food= "trail mix")
+  puts "Hungry before the final course? Thats ok, snack on some #{default_food} to keep you up and at'em for the rest of your day!"
+end
+
+def dinner (default_food= "spagetti")
+  puts "The last meal of the day should be your best, so some nice homecooked #{default_food} should be just the right thing to finish up your night."
+end
+
+breakfast
+lunch
+snack
+dinner
