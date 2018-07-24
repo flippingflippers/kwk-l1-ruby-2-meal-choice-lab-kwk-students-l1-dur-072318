2d2378c2a628b59f8ca8fb0f0f37f811aa7ab935
flippingflippers <citrussunshine2004@gmail.com>
2018-07-24 14:48:20 +0000

    automatically backed up by learn

--- new_meal_choice.rb
+++ new_meal_choice.rb
@@ -14,7 +14,7 @@ def dinner (default_food= "spagetti")
   puts "The last meal of the day should be your best, so some nice homecooked #{default_food} should be just the right thing to finish up your night."
 end
 
-breakfast(bacon with eggs)
+breakfast
 lunch(kraft mac n cheese)
 snack(fruit snacks)
 dinner(roast beef stew)
